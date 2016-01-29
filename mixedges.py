a5294f5867a0b1781fa20ad0ad97371dd3e68c93
Dan Schult <dschult@colgate.edu>
2016-01-28 22:42:21 -0500

    An edge class for both un/directed edges with tests
    Not yet connected to graph/node/etc.
     - data structure is split to _pred, _succ based on initial add
     - Very few methods check G.directed. if structure chosen to make it
       rare: contains, detitem, add, update, remove, discard
     - set operations on ddicts don't work out of the box. Perhaps we have
       to do the set operations on the keys and then hand the ddicts off the
       result.
     - G.e.keys() only offers same interface as G.e     Needed?
     - G.e.data() contains is strange here... only clear method is iter.
                  set operations don't make sense here because not hashable.

---
+++ mixedges.py
@@ -0,0 +1,247 @@
+from collections import MappingView, Set
+from networkx.exception import NetworkXError
+
+# =================
+#  EdgeView Classes
+# =================
+#  Cheap, quick, lightweight access to:
+#    - edge keys (iterate, len, contains) [could do set ops here)
+#    - edge data (iterate, len, contains--but maybe don't need contains?)
+#    - edge items (iterate, len, contains)
+#
+#  Set Operations Broken on data/items
+#    Unlike dictViews, the data and items here always have dicts in them
+#    so we can't easily do set operations.  Perhaps we can do set operations
+#    on the keys portion of items and then hang the dicts off. Not sure 
+#    it makes sense to do sets with dataview.  
+#    Maybe we don't need dataview at all?
+#    The KeysView methods simply call th Edges methods, so maybe dont need
+#    that either.
+#
+class BaseEdgeView(object):
+    __slots__ = ["_edges"]
+    def __init__(self, edges):
+        self._edges = edges
+    def __repr__(self):
+        return '{0.__class__.__name__}({0._edges!r})'.format(self)
+    def __len__(self):
+        # size of graph
+        return len(self._edges)
+
+class EdgeKeys(BaseEdgeView):
+    __slots__ = ["_edges"]
+    def __iter__(self):
+        return iter(self._edges)
+    def __contains__(self, key):
+        return key in self._edges
+
+class EdgeData(BaseEdgeView):
+    __slots__ = ["_edges"]
+    def __iter__(self):
+        for e,d in self._edges._items():
+            yield d
+    def __contains__(self, data):
+        # Do we need/want to provide ability to look up a datadict?
+        # Maybe leave this out?
+        # need to look at all data
+        return any(d == data for d in self)
+
+class EdgeItems(BaseEdgeView):
+    __slots__ = ["_edges"]
+    def __iter__(self):
+        return self._edges._items()
+    def __contains__(self, item):
+        try:
+            print("In items-contains. input:",item)
+            (u,v),d = item # raises for ``eitem`` not ((u,v),d)
+            print("split input")
+            ddict = self._edges[(u,v)]
+            print("got eddict", d==ddict)
+            return d == ddict
+        except:
+            return False
+
+
+class Edges(Set):
+    """ Directed or Undirected Edges class.
+    
+        The two adjacency structures _succ and _pred are split.
+        The order provided on initial addition is the orientation of 
+        storage even for undirected edges.
+
+        ``directed`` is a read-only property. 
+        Behavior of methods is affected by the directed property in
+        __contains__, __getitem__, add, update, remove, discard
+    """
+    __slots__ = ('_node', '_succ', '_pred', '_directed')
+    def __init__(self, node, succ, pred, directed):
+        self._node = node
+        self._succ = succ
+        self._pred = pred
+        self._directed = directed
+
+    @property
+    def directed(self):
+        return self._directed
+
+    @classmethod
+    def _from_iterable(cls, it):
+        '''Adapted from Set class to return a set instead of Edges'''
+        return set(it)
+
+    def __repr__(self):
+        return '{0.__class__.__name__}({1})'.format(self,list(self))
+    def keys(self):
+        return EdgeKeys(self)
+    def data(self):
+        return EdgeData(self)
+    def items(self):
+        return EdgeItems(self)
+    def selfloops(self):
+        return ((n, n) for n, nbrs in self._succ.items() if n in nbrs)
+
+    def __len__(self):
+        """size of graph"""
+        return sum(len(nbrs) for n, nbrs in self._succ.items())
+    def __iter__(self):
+        nodes_nbrs = self._succ.items()
+        for n, nbrs in nodes_nbrs:
+            for nbr in nbrs:
+                yield (n, nbr)
+    def _items(self):
+        # Helper Function for Views and Adjacency
+        nodes_nbrs = self._succ.items()
+        for n, nbrs in nodes_nbrs:
+            for nbr, ddict in nbrs.items():
+                yield (n,nbr),ddict
+    # Affected by self.directed
+    def __contains__(self, key):
+        try:
+            u,v = key
+            if v in self._succ[u]:
+                return True
+            # if not directed check other direction
+            return (not self.directed) and (v in self._pred[u])
+        except:
+            return False
+    def __getitem__(self, key):
+        try:
+            u,v = key
+        except TypeError:
+            raise NetworkXError('bad edge key: use edge key = (u,v)')
+        try:
+            return self._succ[u][v]
+        except KeyError:
+            if self.directed:
+                raise
+            return self._pred[u][v]
+    # Mutating Methods
+    def add(self, u, v, attr_dict=None, **attr):
+        if attr_dict is None:
+            attr_dict = attr
+        else:
+            try:
+                attr_dict.update(attr)
+            except AttributeError:
+                raise NetworkXError(
+                    "The attr_dict argument must be a dictionary.")
+        # add nodes
+        u_new = u not in self._succ
+        v_new = v not in self._succ
+        if u_new:
+            self._succ[u] = {} # fixme factory
+            self._pred[u] = {} # fixme factory
+            self._node[u] = {}
+        if v_new:
+            self._succ[v] = {} # fixme factory
+            self._pred[v] = {} # fixme factory
+            self._node[v] = {}
+        # find the edge
+        if not (u_new or v_new):
+            if v in self._succ[u]:
+                datadict = self._succ[u][v]
+                datadict.update(attr_dict)
+                return
+            # if not directed check other direction
+            if (not self.directed) and v in self._pred[u]:
+                datadict = self._pred[u][v]
+                datadict.update(attr_dict)
+                return
+            # else new edge-- drop out of if
+        # add new edge
+        datadict = {}
+        datadict.update(attr_dict)
+        self._succ[u][v] = datadict
+        self._pred[v][u] = datadict
+
+    def update(self, ebunch, attr_dict=None, **attr):
+        # set up attribute dict
+        if attr_dict is None:
+            attr_dict = attr
+        else:
+            try:
+                attr_dict.update(attr)
+            except AttributeError:
+                raise NetworkXError(
+                    "The attr_dict argument must be a dictionary.")
+        # process ebunch
+        for e in ebunch:
+            ne = len(e)
+            if ne == 3:
+                u, v, dd = e
+            elif ne == 2:
+                u, v = e
+                dd = {}  # doesnt need edge_attr_dict_factory
+            else:
+                msg = "Edge tuple %s must be a 2-tuple or 3-tuple." % (e,)
+                raise NetworkXError(msg)
+            # add nodes
+            u_new = u not in self._succ
+            v_new = v not in self._succ
+            if u_new:
+                self._succ[u] = {} # fixme factory
+                self._pred[u] = {} # fixme factory
+                self._node[u] = {}
+            if v_new:
+                self._succ[v] = {} # fixme factory
+                self._pred[v] = {} # fixme factory
+                self._node[v] = {}
+            # find the edge
+            if not (u_new or v_new):
+                if v in self._succ[u]:
+                    datadict = self._succ[u][v]
+                    datadict.update(attr_dict)
+                    datadict.update(dd)
+                    continue
+                # if not directed check other direction
+                if (not self.directed) and v in self._pred[u]:
+                    datadict = self._pred[u][v]
+                    datadict.update(attr_dict)
+                    datadict.update(dd)
+                    continue
+                # else new edge-- drop out of if
+            # add new edge
+            datadict = {}
+            datadict.update(attr_dict)
+            datadict.update(dd)
+            self._succ[u][v] = datadict
+            self._pred[v][u] = datadict
+    def discard(self, u, v):
+        try:
+            del self._succ[u][v]
+            del self._pred[v][u]
+        except KeyError:
+            pass
+    def remove(self, u, v):
+        try:
+            del self._succ[u][v]
+        except KeyError:
+            raise NetworkXError("The edge %s-%s is not in the graph" % (u, v))
+        try:
+            del self._pred[v][u]
+        except KeyError: # self-loop can cause failure in undirected
+            pass
+    def clear(self):
+        for n in self._succ:
+            self._succ[n].clear()
+            self._pred[n].clear()
---
+++ test_edges.py
@@ -0,0 +1,124 @@
+#
+#   TESTS
+#
+from nose.tools import assert_true, assert_equal, assert_raises
+
+from mixedges import Edges, EdgeKeys, EdgeData, EdgeItems
+
+class BaseEdgeTests(object):
+    def setup_edges(self):
+        self.edlist = [{1:"one"}, {1:"two"}, {1:"three"}, {1:"four"}]
+        ed1, ed2, ed3, ed4 = self.edlist
+        Ge = self.Ge
+        Ge.add(0,1,ed1)
+        Ge.add(0,0,ed2)
+        Ge.update([(1,0,ed3), (2,3,ed4)])
+    def test_iter_items(self):
+        Ge = self.Ge
+        ed1, ed2, ed3, ed4 = self.edlist
+        if Ge.directed:
+            ans = [(0,1), (0,0), (1,0), (2,3)]
+        else:
+            ans = [(0,1), (0,0), (2,3)]
+        assert_equal( sorted(Ge), sorted(ans))
+        if Ge.directed:
+            ans = [((0,1),ed1), ((0,0),ed2), ((1,0),ed3), ((2,3),ed4)]
+        else:
+            ans = [((0,1),ed3), ((0,0),ed2), ((2,3),ed4)]
+        print("succ:",Ge._succ)
+        print("pred:",Ge._pred)
+        print("items",list(Ge._items()))
+        assert_equal( sorted(Ge._items()), sorted(ans))
+    def test_view_data_keys(self):
+        Ge = self.Ge
+        ed1, ed2, ed3, ed4 = self.edlist
+        if Ge.directed:
+            ans = [((0,1),ed1), ((0,0),ed2), ((1,0),ed3), ((2,3),ed4)]
+        else:
+            ans = [((0,1),ed3), ((0,0),ed2), ((2,3),ed4)]
+        # iter
+        assert_equal( sorted(Ge.items()), sorted(ans))
+        assert_equal( sorted(Ge.data()), sorted(d for k,d in ans))
+        assert_equal( sorted(Ge.keys()), sorted(k for k,d in ans))
+        # contains
+        assert_true( (0,1) in Ge.keys() )
+        assert_true( (0,3) not in Ge.keys() )
+        assert_true( (0,8) not in Ge.keys() )
+
+        extras = [((0,1),{1:"none"}), ((2,3),ed4), ((0,8),ed3)]
+        assert_true( ed2 in Ge.data() )
+        assert_true( extras[0][1] not in Ge.data() )
+        assert_true( ((0,0),ed2) in Ge.items() )
+        assert_true( extras[0] not in Ge.items() )
+        assert_true( extras[1] in Ge.items() )
+        assert_true( extras[2] not in Ge.items() )
+
+
+    def test_len(self):
+        Ge = self.Ge
+        assert_equal(len(Ge), 4 if Ge.directed else 3)
+        assert_equal(len(Ge.items()), len(Ge))
+        assert_equal(len(Ge.data()), len(Ge))
+        assert_equal(len(Ge.keys()), len(Ge))
+
+    def test_contains_get(self):
+        Ge = self.Ge
+        ed1, ed2, ed3, ed4 = self.edlist
+        assert_true((0,1) in Ge)
+        assert_true((1,0) in Ge)
+        assert_true((2,3) in Ge)
+        assert_true((0,0) in Ge)
+        if Ge.directed:
+            assert_true((3,2) not in Ge)
+        else:
+            assert_true((3,2) in Ge)
+        assert_true((4,5) not in Ge)
+        assert_true((4,4) not in Ge)
+        # getitem
+        assert_true(Ge[(0,1)] == (ed1 if Ge.directed else ed3))
+        assert_true(Ge[(1,0)] == ed3)
+        assert_true(Ge[(2,3)] == ed4)
+        assert_true(Ge[(0,0)] == ed2)
+
+    def test_remove_clear(self):
+        Ge = self.Ge
+        Ge.remove(0,1)
+        assert_true((0,1) not in Ge)
+        if Ge.directed:
+            assert_true((1,0) in Ge)
+        else:
+            assert_true((1,0) not in Ge)
+        Ge.clear()
+        assert_equal(len(Ge._node), 5)
+        assert_equal(len(Ge), 0)
+
+    def test_set_ops(self):
+        Ge = self.Ge
+        extras = [(1,2), (0,1), (3,4)]
+        if Ge.directed:
+            edgs = [(0,1), (0,0), (1,0), (2,3)]
+        else:
+            edgs = [(0,1), (0,0), (2,3)]
+        assert_equal(Ge | extras, set(edgs) | set(extras) )
+        assert_equal(Ge & extras, set(edgs) & set(extras) )
+        assert_equal(Ge ^ extras, set(edgs) ^ set(extras) )
+        assert_equal(Ge - extras, set(edgs) - set(extras) )
+        assert_equal(extras - Ge, set(extras) - set(edgs) )
+
+class TestDiEdges(BaseEdgeTests):
+    def setUp(self):
+        node ={4:{}}
+        succ = {}
+        pred = {}
+        self.Ge = Edges(node, succ, pred, directed=False)
+        self.setup_edges()
+class TestUndiEdges(BaseEdgeTests):
+    def setUp(self):
+        node ={4:{}}
+        succ = {}
+        pred = {}
+        self.Ge = Edges(node, succ, pred, directed=False)
+        self.setup_edges()
+        self.setup_edges()
+
+
